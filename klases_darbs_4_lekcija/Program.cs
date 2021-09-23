6954dc90fd2f69f4053284be3c2b7a971cba4cd1
Edvarts Krakts <edvarts.krakts@gmail.com>
2021-09-23 14:42:21 +0300

    test

---
+++ klases_darbs_4_lekcija/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace klases_darbs_4_lekcija
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //  bool isTrue = true;
+            //  if (isTrue) = true
+            //           { Console.WriteLine("tekst"); }
+
+            //  else
+            //   { Console.WriteLine("cits teksts"); }
+
+            //   int age = 70;
+            //  if (age < 18)
+            //    { Console.WriteLine("Tu esi jaunietis"); }
+            //     else if (age >= 70)
+            //  { Console.WriteLine("Pensionārs)"; }
+            //    else {Console.WriteLine("Tu esi darbspējīgā vecumā)"; }
+
+            int abcde = int.Parse(Console.ReadLine());
+            switch (abcde)
+            {
+                case 1:   // te salīdzina, vai pie case minētais skaitlis ir tas, kas ir augšā pie svitča
+                    Console.WriteLine("Case1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Case2");
+                    break;
+                case 3:
+                    Console.WriteLine("Case3");
+                    break;
+
+                default:
+                    Console.WriteLine("kaut kas cits");
+                    break;
+
+
+            }
+
+            string name = "Artis";
+         
+            switch (name)
+            {
+                case "a":   // te salīdzina, vai pie case minētais skaitlis ir tas, kas ir augšā pie svitča
+                    Console.WriteLine("Case1");
+                    break;
+
+                case "b":
+                    Console.WriteLine("Case2");
+                    break;
+                case "Artis":
+                    Console.WriteLine("Case3");
+                    break;
+
+                default:
+                    Console.WriteLine("kaut kas cits");
+                    break;
+
+
+            }
+
+
+
+        }
+    }
+}
