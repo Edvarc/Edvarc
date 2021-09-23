0435ebca71e2c222ad9b9b80273a3c2b9fbaa853
Edvarts Krakts <edvarts.krakts@gmail.com>
2021-09-23 15:06:35 +0300

    tetsttetwe tt

--- majasdar_4/majasdarb4_v5/Program.cs
+++ majasdar_4/majasdarb4_v5/Program.cs
@@ -7,6 +7,7 @@ namespace majasdarb4_v5
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
